a191c2345a06e9e09b589b15b5b63ee328f4c390
LUC TERRACHER <lucterracher@gmail.com>
2015-10-17 03:42:51 +0200

    move main js file + edit html + add angular-material

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', ['ngMaterial']);
+
+myApp.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
+}]);
